9f252de4a374ef7aaca11b0ae2ccf9aff69c1f72
mike <859897994@qq.com>
2018-05-31 05:01:18 +0800

    add some script

---
+++ source/arch-script/yaourt.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+echo "setting yaourt=============================>>"
+if ( grep 'archlinuxcn' /etc/pacman.conf)
+then
+	archlinuxcn is exist
+else
+	echo -e '
+[archlinuxcn]
+SigLevel=Never
+Server = http://repo.archlinuxcn.org/$arch
+' >> /etc/pacman.conf
+fi
+MY_INDEX=0
+while (( $MY_INDEX <= 6 ))
+do
+pacman -Sy --noconfirm --needed yaourt fakeroot archlinuxcn-keyring screenfetch ttf-dejavu ttf-droid wqy-microhei wqy-zenhei google-chrome fcitx fcitx-im fcitx-libpinyin fcitx-googlepinyin fcitx-configtool
+if [[ $? -eq 0 ]]
+then
+	break
+fi
+done
+#============for fcitx================
+echo -e "\n\n========fcitx setting==========\n\n"
+if [[ `cat /etc/profile | grep fcitx` ]]
+then
+	echo found fcitx in /etc/porfile
+else
+
+	echo 000000 | sudo -S chmod 777 /etc/profile
+	echo 'export XMODIFIERS="@im=fcitx"
+export GTK_IM_MODULE="fcitx"
+export QT_IM_MODULE="fcitx"' >> /etc/profile
+	echo 000000 | sudo -S chmod 644 /etc/profile
+	echo done
+fi
+
+su mike -c 'git clone https://github.com/tignioj/linux.git ~/clone/linux
+~/clone/linux/config/total.sh
+screenfetch'
+
+echo "done"
